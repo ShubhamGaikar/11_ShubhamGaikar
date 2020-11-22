dc0bf7a3ad7d61bbbbd2b915d72e21a4e1240fab
ShubhamGaikar <73989886+ShubhamGaikar@users.noreply.github.com>
2020-11-23 02:34:12 +0530

    Create Pract6.cpp

---
+++ Pract6.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class greetings
+{
+ public: void display(){
+ cout<<"Have a nice day:)";
+ }
+};
+int main()
+{
+ greetings obj;
+ obj.display();
+ return 0;
+}
