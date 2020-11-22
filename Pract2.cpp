4796b0ace6daeee81e88877b043f8387b9c3312b
ShubhamGaikar <73989886+ShubhamGaikar@users.noreply.github.com>
2020-11-23 02:28:53 +0530

    Update Pract2.cpp

--- Pract2.cpp
+++ Pract2.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-
 using namespace std;
-
-int main(){
-	int x;
-	cout<<"Enter the number:";
-	cin>>x;
-	cout<<"You entered:" <<x <<"\n";
-	}
+int main() {
+ int num;
+ cout<<"Enter the number:¥n";
+ cin>>num;
+ cout<<"The number entered by user is "<<num;
+ return 0;
+}
